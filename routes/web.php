62eaecc23b5c36ce033ecbcccc3ea960f6fa3f84
KavinduTheekshana <kavindutheekshana@gmail.com>
2019-07-20 18:50:52 +0530

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// Route::GET('/', 'LoginController@showLoginForm')->name('admin.login');
+Route::get('/', function () {
+    return view('vendor.multiauth.admin.login');
+});
