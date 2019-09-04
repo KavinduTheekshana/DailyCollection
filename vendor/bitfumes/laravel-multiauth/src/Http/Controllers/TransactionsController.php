ec2990c95ca4785318208c7ac582b0b3124adf4f
Kavindu Theekshana <kavindutheekshana@gmail.com>
2019-09-04 09:57:53 +0530

    transactionerrorfixnew

--- vendor/bitfumes/laravel-multiauth/src/Http/Controllers/TransactionsController.php
+++ vendor/bitfumes/laravel-multiauth/src/Http/Controllers/TransactionsController.php
@@ -18,7 +18,7 @@ class TransactionsController extends Controller
     public function __construct()
     {
         $this->middleware('auth:admin');
-        $this->middleware('role:super', ['only' => 'show']);
+        // $this->middleware('role:super', ['only' => 'show']);
     }
 
     public function show()
